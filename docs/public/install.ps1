826c7ab67bba03025691dc4131df948b71de0c73
Aaron Parker <aaron@stealthpuppy.com>
2025-11-27 16:30:33 +1100

    Add architecture detection to install script
    The install.ps1 script now detects system architecture (AMD64 or ARM64) and installs the appropriate Visual C++ Redistributables. Also updated copyright year and removed unnecessary error handling for PSGallery trust.

--- docs/public/install.ps1
+++ docs/public/install.ps1
@@ -7,7 +7,7 @@
     Installs the VcRedist PowerShell module and installs the default Microsoft Visual C++ Redistributables on the local system.
 
     .NOTES
-    Copyright 2023, Aaron Parker, stealthpuppy.com
+    Copyright 2025, Aaron Parker, stealthpuppy.com
 #>
 [CmdletBinding()]
 param (
@@ -18,14 +18,7 @@ param (
 #region Trust the PSGallery for modules
 $Repository = "PSGallery"
 if (Get-PSRepository | Where-Object { $_.Name -eq $Repository -and $_.InstallationPolicy -ne "Trusted" }) {
-    try {
-        Write-Host "Trusting the repository: $Repository."
-        Install-PackageProvider -Name "NuGet" -MinimumVersion 2.8.5.208 -Force
         Set-PSRepository -Name $Repository -InstallationPolicy "Trusted"
-    }
-    catch {
-        throw $_
-    }
 }
 #region
 
@@ -48,13 +41,19 @@ if (($null -eq $installedModule) -or ([System.Version]$publishedModule.Version -
 }
 #endregion
 
-
 #region tasks/install apps
 Write-Host "Saving VcRedists to path: $Path."
 New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" > $null
 
 Write-Host "Downloading and installing supported Microsoft Visual C++ Redistributables."
-$Redists = Get-VcList | Save-VcRedist -Path $Path | Install-VcRedist -Silent
+if ($Env:PROCESSOR_ARCHITECTURE -eq "AMD64") {
+    Write-Host "64-bit system detected."
+    $Architecture = "x86", "x64"
+} elseif ($Env:PROCESSOR_ARCHITECTURE -eq "ARM64") {
+    Write-Host "ARM64 system detected."
+    $Architecture = "x86", "x64", "arm64"
+}
+$Redists = Get-VcList -Architecture $Architecture | Save-VcRedist -Path $Path | Install-VcRedist -Silent
 
 Write-Host "Installed Visual C++ Redistributables:"
 $Redists | Select-Object -Property "Name", "Release", "Architecture", "Version" -Unique
